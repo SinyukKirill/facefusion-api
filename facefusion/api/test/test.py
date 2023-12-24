d2e3d495dc630afff86b5f5ff281a90ddd79c466
tamoharu <133945583+tamoharu@users.noreply.github.com>
2023-12-25 03:15:02 +0900

    修正

--- facefusion/api/test/test.py
+++ facefusion/api/test/test.py
@@ -8,26 +8,26 @@ def image_to_base64_str(image_path):
         return encoded_string.decode('utf-8')
 
 source_image_path = 'source.jpg'
-target_image_path = 'test-2.mp4'
+target_image_path = 'test.jpg'
 
 source_str = image_to_base64_str(source_image_path)
 target_str = image_to_base64_str(target_image_path)
 
 params = {
     'user_id': 'test',
-    # 'source': source_str,
+    'source': source_str,
     'target': target_str,
-    # 'source_type': 'jpg',
-    'target_type': 'mp4',
-    'frame_processors': ['face_blur'],
-    'face_mask_blur': 0.5,
-    'face_mask_padding': [5, 5, 5, 5],
-    'keep_fps': True,
-    'execution_thread_count': 40,
-    'face_selector_mode': 'one',
+    'source_type': 'jpg',
+    'target_type': 'jpg',
+    'frame_processors': ['face_swapper'],
+    # 'face_mask_blur': 0.5,
+    # 'face_mask_padding': [5, 5, 5, 5],
+    # 'keep_fps': True,
+    # 'execution_thread_count': 40,
+    # 'face_selector_mode': 'one',
 }
 
-url = 'https://a8b7-2400-4050-b6e0-1600-c402-983e-4379-906c.ngrok-free.app/'
+url = 'https://e082-221-103-234-215.ngrok-free.app/'
 response = requests.post(url, json=params)
 
 print("Status Code:", response.status_code)
